a2b29f7d9cc79c1b94e0c90dc6e01d9ad33cc556
Peter <peter@lighthouse16.com>
2020-10-04 06:43:18 +0200

    Publish as a dependancy

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.5.0")
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NoThemeForPublish",
-            dependencies: []),
+            dependencies: ["Publish"]),
     ]
 )
